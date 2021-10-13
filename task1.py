023fcf6de8babbc709305adbdd84a1ecbe20da6c
howuvebeenkeeping <rigabdullin@yandex.ru>
2021-10-13 15:16:08 +0500

    refactor task1 and task2

--- task1.py
+++ task1.py
@@ -2,34 +2,41 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+ipstack_access_key = '0839b08d814cd50d1f6cce783ff85924'
 url = 'https://ru.wikipedia.org/w/index.php?title=JSON&action=history'
+ipv4_regex = r'(?:(?:(?:1?[1-9]?|10|2[0-4])\d|25[0-5])\.){3}(?:(?:1?[1-9]?|10|2[0-4])\d|25[0-5])'
+country_users_count = {}
+bdis = set()
+
 wiki_request = requests.get(url)
 soup = BeautifulSoup(wiki_request.content, 'html.parser')
 anchors = soup.find_all("a", {'class': 'mw-userlink mw-anonuserlink'})
-bdis = set()
 
-for anchor in anchors:
-    bdi = anchor.find('bdi')
-    if bdi is not None:
-        bdis.add(bdi.get_text())
-
-ipv4_regex = r'(?:(?:(?:1?[1-9]?|10|2[0-4])\d|25[0-5])\.){3}(?:(?:1?[1-9]?|10|2[0-4])\d|25[0-5])'
-bdis_as_string = ' '.join(bdis)
-ips = re.findall(ipv4_regex, bdis_as_string)
-print('IP-addresses:', *ips, sep='\n', end='\n\n')
-
-country_users_count = {}
-
-for ip in ips:
-    url = 'http://api.ipstack.com/{0}?access_key=0839b08d814cd50d1f6cce783ff85924'.format(ip)
-    ipstack_request = requests.get(url)
-    country_name = ipstack_request.json()['country_name']
-    if country_name not in country_users_count.keys():
-        country_users_count[country_name] = 1
+if anchors is not None:
+    for anchor in anchors:
+        bdi = anchor.find('bdi')
+        if bdi is not None:
+            bdis.add(bdi.get_text())
+    if len(bdis) != 0:
+        ips = re.findall(ipv4_regex, ' '.join(bdis))
+        if len(ips) != 0:
+            print('IP-адреса:', *ips, sep='\n')
+            print('\nОпределение стран и количества пользователей...')
+            for ip in ips:
+                url = f'http://api.ipstack.com/{ip}?access_key={ipstack_access_key}'
+                ipstack_request = requests.get(url)
+                country_name = ipstack_request.json()['country_name']
+                if country_name not in country_users_count.keys():
+                    country_users_count[country_name] = 1
+                else:
+                    country_users_count[country_name] += 1
+            longest_country_name = max([len(country_name) for country_name in country_users_count.keys()])
+            for country_name, users_count in country_users_count.items():
+                separator = ' ' * (longest_country_name - len(country_name) + 2)
+                print(country_name, users_count, sep=separator)
+        else:
+            print('IP-адреса не найдены')
     else:
-        country_users_count[country_name] += 1
-
-longest_country_name = max([len(country_name) for country_name in country_users_count.keys()])
-for country_name, users_count in country_users_count.items():
-    separator = ' ' * (longest_country_name - len(country_name) + 2)
-    print(country_name, users_count, sep=separator)
+        print('Теги <bdi> не найдены')
+else:
+    print('Ссылки не найдены')
--- task2.py
+++ task2.py
@@ -1,37 +1,39 @@
 import requests
 
 vacancies = []
-per_page = 100
 pages_count = 19
 
 for page_idx in range(pages_count):
-    url = 'https://api.hh.ru/vacancies?industry=7&per_page={0}&page={1}'.format(per_page, page_idx)
-    request = requests.get(url)
-    items = request.json()['items']
-    if items:
+    url = f'https://api.hh.ru/vacancies?industry=7&per_page=100&page={page_idx}'
+    hhru_request = requests.get(url)
+    items = hhru_request.json()['items']
+    if items is not None:
         vacancies.extend(items)
-    print('Загружено {} вакансий'.format(per_page * (page_idx + 1)))
+        print(f'Загружено {len(vacancies)} вакансий')
 
-while True:
-    salaries = []
-    keyword = input('\nВведите ключевое слово: ')
-    if keyword.lower().strip() == 'q':
-        break
-    print('\nВакансии:')
-    for vacancy in vacancies:
-        vacancy_name = vacancy['name'].replace('(', ' ').replace(')', ' ')
-        if keyword.lower() in vacancy_name.lower().split():
-            salary = vacancy['salary']
-            print(vacancy_name)
-            if salary:
-                if salary['from'] is not None and salary['to'] is None:
-                    salaries.append(salary['from'])
-                elif salary['from'] is None and salary['to'] is not None:
-                    salaries.append(salary['to'])
-                elif salary['from'] is not None and salary['to'] is not None:
-                    salaries.append((salary['to'] + salary['from']) / 2)
+if len(vacancies) != 0:
+    while True:
+        salaries = []
+        keyword = input('\nВведите ключевое слово: ')
+        if keyword.lower().strip() == 'q':
+            break
+        for vacancy in vacancies:
+            vacancy_name = vacancy['name'].replace('(', ' ').replace(')', ' ')
+            if keyword.lower() in vacancy_name.lower().split():
+                salary = vacancy['salary']
+                if salary is not None:
+                    if salary['from'] is not None and salary['to'] is None:
+                        salaries.append(salary['from'])
+                    elif salary['from'] is None and salary['to'] is not None:
+                        salaries.append(salary['to'])
+                    elif salary['from'] is not None and salary['to'] is not None:
+                        salaries.append((salary['to'] + salary['from']) / 2)
+                    print(vacancy_name)
 
-    if len(salaries) != 0:
-        print('\nСредняя З/П по ключевому слову {}:'.format(keyword), round(sum(salaries) / len(salaries)), 'рублей')
-    else:
-        print('Ничего не найдено')
+        if len(salaries) != 0:
+            average_salary = round(sum(salaries) / len(salaries))
+            print(f'\nСредняя З/П по ключевому слову {keyword}:', average_salary, 'рублей')
+        else:
+            print('Вакансии не найдены или З/П не указаны')
+else:
+    print('Ни одной вакансии не найдено')
